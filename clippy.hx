eacd7157f040f8afc028bf3223716bc4e63650e2
Joseph Andaverde <joseph.andaverde@softekinc.com>
2013-10-10 12:54:44 -0500

    tried to remove text

--- clippy.hx
+++ clippy.hx
@@ -9,23 +9,7 @@ class Clippy {
   // Main
   static function main() {
     var text:String = flash.Lib.current.loaderInfo.parameters.text;
-    
-    // label
-    
-    var label:TextField = new TextField();
-    var format:TextFormat = new TextFormat("Arial", 10);
-    
-    label.text = "copy to clipboard";
-    label.setTextFormat(format);
-    label.textColor = 0x888888;
-    label.selectable = false;
-    label.x = 15;
-    label.visible = false;
-    
-    flash.Lib.current.addChild(label);
-    
-    // button
-    
+  
     var button:SimpleButton = new SimpleButton();
     button.useHandCursor = true;
     button.upState = flash.Lib.attach("button_up");
@@ -35,20 +19,8 @@ class Clippy {
     
     button.addEventListener(MouseEvent.MOUSE_UP, function(e:MouseEvent) {
       flash.system.System.setClipboard(text);
-      label.text = "copied!";
-      label.setTextFormat(format);
     });
-    
-    button.addEventListener(MouseEvent.MOUSE_OVER, function(e:MouseEvent) {
-      label.visible = true;
-    });
-    
-    button.addEventListener(MouseEvent.MOUSE_OUT, function(e:MouseEvent) {
-      label.visible = false;
-      label.text = "copy to clipboard";
-      label.setTextFormat(format);
-    });
-    
+
     flash.Lib.current.addChild(button);
   }
 }
